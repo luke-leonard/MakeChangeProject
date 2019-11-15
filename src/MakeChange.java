1c4fb7eab55e16c46b1d22e45af0a27bd0736188
Luke Leonard (Carly) <lukeleonard.coder@gmail.com>
2019-11-15 15:39:27 -0700

    Initial Commit

---
+++ src/MakeChange.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class MakeChange {
+	public static Scanner input;
+
+	public static void main(String[] args) {
+		input = new Scanner((System.in));
+		
+		
+		input.close();
+	}
+
+}
